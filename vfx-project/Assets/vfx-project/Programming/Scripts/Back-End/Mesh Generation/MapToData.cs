2ae27fa9edff4c1e300d2fe00a637cd220a39d27
Christopher Robertson <christopherprobertson@gmail.com>
2020-11-21 18:16:34 +0000

    - Have it successfully drawing cubes generated at runtime with the bottom of the cube removed. I intend to remove the faces that cannot be seen from any direction (except the bottom.
    - If I have time I might go about welding vertices, but I shall see.

--- vfx-project/Assets/vfx-project/Programming/Scripts/Back-End/Mesh Generation/MapToData.cs
+++ vfx-project/Assets/vfx-project/Programming/Scripts/Back-End/Mesh Generation/MapToData.cs
@@ -12,6 +12,8 @@ namespace VFX.MeshGeneration
         private float tileYSize = 1.0f;
         [SerializeField]
         private float yHeightScalar = 1.0f;
+        [SerializeField]
+        private float roundedYPos = 5.0f;
         private Vector3[,] tilePositions = null;
         private List<Vector3> positions = new List<Vector3>();
 
@@ -20,41 +22,91 @@ namespace VFX.MeshGeneration
         private Texture2D noiseMap = null;
 
         [SerializeField]
+        private Material tileMaterial = null;
+
+        [SerializeField]
         private bool drawDebug = false;
 
         private void Start()
         {
             tilePositions = GetPositionsFromTexture(noiseMap);
-
-            CreateVerticesAndTriangles();
+            CreateTiles();
             
         }
 
         // Source: https://catlikecoding.com/unity/tutorials/procedural-grid/
-        private void CreateVerticesAndTriangles()
+        private void CreateTiles()
         {
-            Mesh plane = CreatePlane(Vector3.zero);
-            GameObject tile = new GameObject("Tile");
 
-            tile.AddComponent<MeshFilter>().mesh = plane;
-            tile.AddComponent<MeshRenderer>();
+            GameObject parent = new GameObject("Map");
+            foreach (Vector3 pos in tilePositions)
+            {
+                Mesh plane = CreateCube(pos);
+                GameObject tile = new GameObject("Tile");
+
+                tile.AddComponent<MeshFilter>().mesh = plane;
+                tile.AddComponent<MeshRenderer>().material = tileMaterial;
+
+                tile.transform.position = pos;
+                tile.transform.SetParent(parent.transform);
+            }
         }
 
-        public Mesh CreatePlane(Vector3 centrePos)
+        // Source: http://ilkinulas.github.io/development/unity/2016/04/30/cube-mesh-in-unity3d.html
+        // Source: https://gist.github.com/prucha/866b9535d525adc984c4fe883e73a6c7
+        public Mesh CreateCube(Vector3 centrePos)
         {
             Mesh plane = new Mesh();
 
-            Vector3[] vertices = GetVerticesFromCentre(centrePos);
-            int[] tris = new int[6]
+            float width = tileXSize * .5f;
+            float height = tileYSize * .5f;
+            float length = tileXSize * .5f;
+
+            Vector3[] coords =
             {
-                0, 1, 2,
-                1, 3, 2
+                new Vector3(-length, -width, height),
+                new Vector3(length, -width, height),
+                new Vector3(length, -width, -height),
+                new Vector3(-length, -width, -height),
+
+                new Vector3(-length, width, height),
+                new Vector3(length, width, height),
+                new Vector3(length, width, -height),
+                new Vector3(-length, width, -height)
+            };
+            
+            Vector3[] vertices =
+            {
+                coords[0], coords[1], coords[2], coords[3], // Bottom
+	            coords[7], coords[4], coords[0], coords[3], // Left
+	            coords[4], coords[5], coords[1], coords[0], // Front
+	            coords[6], coords[7], coords[3], coords[2], // Back
+	            coords[5], coords[6], coords[2], coords[1], // Right
+	            coords[7], coords[6], coords[5], coords[4]  // Top
+            };
+
+            Vector3[] normals =
+            {
+                Vector3.down, Vector3.down, Vector3.down, Vector3.down,
+                Vector3.left, Vector3.left, Vector3.left, Vector3.left,
+                Vector3.forward, Vector3.forward, Vector3.forward, Vector3.forward,
+                Vector3.back, Vector3.back, Vector3.back, Vector3.back,
+                Vector3.right, Vector3.right, Vector3.right, Vector3.right,
+                Vector3.up, Vector3.up, Vector3.up, Vector3.up
+            };
+
+            int[] tris =
+            {
+	            7, 5, 4,        7, 6, 5,        // Left
+	            11, 9, 8,       11, 10, 9,      // Front
+	            15, 13, 12,     15, 14, 13,     // Back
+	            19, 17, 16,     19, 18, 17,	    // Right
+	            23, 21, 20,     23, 22, 21,	    // Top
             };
 
             plane.vertices = vertices;
             plane.triangles = tris;
-
-            
+            plane.normals = normals;
 
             return plane;
         }
@@ -85,7 +137,7 @@ namespace VFX.MeshGeneration
                 {
                     // Setting the initial position taking into account the tile size
                     Vector3 position = new Vector3(x * tileXSize, GetYHeight(x, y, texture), y * tileYSize);
-
+                    position.y = Mathf.Round(position.y / roundedYPos) * roundedYPos;
                     // Offsetting the position by its max size - a tile halved
                     // This makes (0, 0, 0) the world centre of the object
                     position -= (maxWorldSize - new Vector3(tileXSize, 0.0f, tileYSize)) * .5f;

cef82692331315538e9a5c62cd901a49d10b3933
Christopher Robertson <christopherprobertson@gmail.com>
2020-11-21 17:32:13 +0000

    - Have a singular tile being successfully drawn.

---
+++ vfx-project/Assets/vfx-project/Programming/Scripts/Back-End/Mesh Generation/MapToData.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace VFX.MeshGeneration
+{
+    public class MapToData : MonoBehaviour
+    {
+        [Header("Tile Options")]
+        [SerializeField]
+        private float tileXSize = 1.0f;
+        [SerializeField]
+        private float tileYSize = 1.0f;
+        [SerializeField]
+        private float yHeightScalar = 1.0f;
+        private Vector3[,] tilePositions = null;
+        private List<Vector3> positions = new List<Vector3>();
+
+        [Header("Noise")]
+        [SerializeField]
+        private Texture2D noiseMap = null;
+
+        [SerializeField]
+        private bool drawDebug = false;
+
+        private void Start()
+        {
+            tilePositions = GetPositionsFromTexture(noiseMap);
+
+            CreateVerticesAndTriangles();
+            
+        }
+
+        // Source: https://catlikecoding.com/unity/tutorials/procedural-grid/
+        private void CreateVerticesAndTriangles()
+        {
+            Mesh plane = CreatePlane(Vector3.zero);
+            GameObject tile = new GameObject("Tile");
+
+            tile.AddComponent<MeshFilter>().mesh = plane;
+            tile.AddComponent<MeshRenderer>();
+        }
+
+        public Mesh CreatePlane(Vector3 centrePos)
+        {
+            Mesh plane = new Mesh();
+
+            Vector3[] vertices = GetVerticesFromCentre(centrePos);
+            int[] tris = new int[6]
+            {
+                0, 1, 2,
+                1, 3, 2
+            };
+
+            plane.vertices = vertices;
+            plane.triangles = tris;
+
+            
+
+            return plane;
+        }
+
+        // This is essentially the bounding box of the position
+        private Vector3[] GetVerticesFromCentre(Vector3 pos)
+        {
+            Vector3[] corners = new Vector3[4];
+            float halfXSize = tileXSize * .5f;
+            float halfYSize = tileYSize * .5f;
+
+            corners[0] = pos + new Vector3(-halfXSize, 0.0f, -halfYSize); // Bottom Left
+            corners[1] = pos + new Vector3(-halfXSize, 0.0f, halfYSize); // Top Left
+            corners[2] = pos + new Vector3(halfXSize, 0.0f, -halfYSize); // Bottom Right
+            corners[3] = pos + new Vector3(halfXSize, 0.0f, halfYSize); // Top Right
+
+            return corners;
+        }
+
+        #region Noise Map Reading
+        private Vector3[,] GetPositionsFromTexture(Texture2D texture)
+        {
+            Vector3 maxWorldSize = new Vector3(texture.width * tileXSize, 0.0f, texture.height * tileYSize);
+            Vector3[,] positions = new Vector3[texture.width, texture.height];
+            for (int y = 0; y < texture.height; y++)
+            {
+                for (int x = 0; x < texture.width; x++)
+                {
+                    // Setting the initial position taking into account the tile size
+                    Vector3 position = new Vector3(x * tileXSize, GetYHeight(x, y, texture), y * tileYSize);
+
+                    // Offsetting the position by its max size - a tile halved
+                    // This makes (0, 0, 0) the world centre of the object
+                    position -= (maxWorldSize - new Vector3(tileXSize, 0.0f, tileYSize)) * .5f;
+                    positions[x, y] = position;
+
+                    //GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = position;
+                }
+            }
+            return positions;
+        }
+
+        private float GetYHeight(int x, int y, Texture2D texture)
+        {
+            Color colour = texture.GetPixel(x, y);
+            return colour.r * yHeightScalar;
+        }
+        #endregion
+
+        #region Debug
+        private void OnDrawGizmos()
+        {
+            if (tilePositions != null && drawDebug)
+            {
+                foreach (Vector3 pos in tilePositions)
+                {
+                    Gizmos.color = Color.green;
+                    Gizmos.DrawWireCube(pos - (Vector3.up * tileYSize * .5f), new Vector3(tileXSize, 1.0f, tileYSize));
+                    Gizmos.DrawSphere(pos, .1f);
+
+                    Vector3[] corners = GetVerticesFromCentre(pos);
+                    foreach (Vector3 e in corners)
+                    {
+                        Gizmos.color = Color.red;
+                        Gizmos.DrawSphere(e, .1f);
+                    }
+                }
+            }
+        }
+        #endregion
+
+
+    }
+}
